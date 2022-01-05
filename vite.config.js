f98fd7413a2b2f6de1d4b64d30d06fe6cec36003
James Teng <jamest.1402@gmail.com>
2022-01-06 04:49:26 +1100

    add all files to github

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite'
+import { resolve } from 'path'
+
+module.exports = defineConfig({
+    base: '/ucbs-v3/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about/about.html'),
+                team: resolve(__dirname, 'team/team.html'),
+                events: resolve(__dirname, 'events/events.html'),
+                sponsors: resolve(__dirname, "sponsors/sponsors.html")
+            }
+        }
+    }
+})
