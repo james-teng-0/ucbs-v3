eb1f8b7045d3c5015169cad6a547417aac013bb1
Jayden Ly <jayden.ly@student.unsw.edu.au>
2022-02-07 03:02:26 +1100

    hi

---
+++ team/team.js
@@ -0,0 +1,19 @@
+const apiResult = [
+	{
+		title: "title1",
+		description: "desc1",
+		output: "out1",
+	},
+	{
+		title: "title2",
+		description: "desc2",
+		output: "out2",
+	},
+	{
+		title: "title3",
+		description: "desc3",
+		output: "out3",
+	},
+];
+
+const container = document.getElementById('')
