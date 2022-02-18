2e440a5f504d7922db31d53166dd4ad73c04d95e
James Teng <jamest.1402@gmail.com>
2022-02-18 23:00:18 +1100

    fix scrolling issue

--- team/team.js
+++ team/team.js
@@ -281,6 +281,13 @@ document.addEventListener("DOMContentLoaded", () => {
 // 	myFunction()
 // };
 
-if ($(window).width() > 991) {
-	$(".sticky").stick_in_parent()
-}
+window.addEventListener('resize', function(event){
+	// do stuff here
+	if ($(window).width() > 991) {
+		$(".sticky").stick_in_parent()
+	}
+	else {
+		$(".sticky").trigger("sticky_kit:detach");
+	}
+});
+
